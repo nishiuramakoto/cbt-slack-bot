c1c10bb0dd9d3f25513f147b2415f284a365eac1
Makoto Nishiura <nishiuramakoto@gmail.com>
2017-12-13 18:22:11 +0900

    fix wrong indentation

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,7 @@ import           Data.Text
 import           Lib
 
 apiKey :: Text
-apiKey = "xoxp-283421821111-281832874353-283270330902-9644d36b22c20f7ae829f38e2b1fd114"
+apiKey = undefined
 
 main :: IO ()
 main = cbtBot apiKey
